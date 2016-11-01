4e2008450b12a677b3be2d3c46b559fc52b7e603
mahendra-16 <prajapatimahendra@ymail.com>
2016-11-01 20:27:01 +0530

    look down

---
+++ Assignment3.java
@@ -0,0 +1,13 @@
+
+public class Assignment3 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		// Data type "char" and "boolean" that cannot be stored in a variable type integer
+		
+		System.out.println("Data type 'char' and 'boolean' that cannot be stored in a variable type integer");
+
+	}
+
+}
